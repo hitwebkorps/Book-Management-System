02f9f5b596366948e3d7bf8e6ee00d2074a99a99
hitwebkorps <hitanshi@webkorps.com>
2025-08-23 02:53:21 +0530

    Update google_repo.py

--- repository/google_repo.py
+++ repository/google_repo.py
@@ -1,6 +1,6 @@
 import requests
-
-Google_API_KEY = "AIzaSyDPD165jbXiE-oKObdwIS8wjpnILzxgO-k"
+import os
+Google_API_KEY = os.environ.get("GOOGLE_BOOKS_API_KEY")
 
 def fetch_books_from_google(query: str):
     url = f"https://www.googleapis.com/books/v1/volumes?q={query}&key={Google_API_KEY}"
